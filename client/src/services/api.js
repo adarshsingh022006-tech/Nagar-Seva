5b8ba7db9a907583eda9eb29add055f0bccc542e
Adarsh Singh <adarshsingh022006@gmail.com>
2026-08-30 10:55:32 +0530

    Fix production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@
 
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({ baseURL: "https://nagar-seva-099u.onrender.com/api" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("nagarseva_token");
