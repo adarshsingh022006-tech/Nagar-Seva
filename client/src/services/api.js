e084d43247cfcedef035e14e721410173ab5dd7e
Adarsh Singh <adarshsingh022006@gmail.com>
2026-08-30 12:02:31 +0530

    Update production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@
 
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://nagar-seva-099u.onrender.com/api" });
+const api = axios.create({ baseURL: "https://nagar-seva-api.onrender.com/api" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("nagarseva_token");
